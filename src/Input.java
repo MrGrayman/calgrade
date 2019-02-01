8f44a3b8b3fa350edcfd25ab3f5cedb1df9df0c9
MrGrayman <gunboy.2541@gmail.com>
2019-02-01 17:19:43 +0700

    create Input

--- src/Input.java
+++ src/Input.java
@@ -1,4 +1,15 @@
-
+import java.util.Scanner;
 public class Input {
-
+	Scanner in = new Scanner(System.in);
+	public int inInt() {
+		return in.nextInt();
+	}
+	
+	public double inDouble() {
+		return in.nextDouble();
+	}
+	
+	public float inFloat() {
+		return in.nextFloat();
+	}
 }
