a5752752a09803f8517c58973ef2befedd098291
mekpongsathon <mek.pongsathon@gmail.com>
2019-02-01 17:48:42 +0700

    push process

--- src/Process.java
+++ src/Process.java
@@ -1,4 +1,24 @@
-
 public class Process {
+	int score;
 
+	public Process(int score) {
+		
+		this.score = score;
+	}
+	public char calculate() {
+		char grade;
+		if(score>=80 )
+			grade = 'A';
+		else if(score>=70)
+			grade = 'B';
+		else if(score>=60)
+			grade = 'C';
+		else if(score>=50)
+			grade = 'D';
+		else grade = 'F';
+		return grade;
+	}
+	
+	
 }
+
